8a95f4193f6209c0d9ad299e5518056fd21f4ed5
oliver-enede <78216854+oliver-enede@users.noreply.github.com>
2024-06-04 10:53:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DijkstraShortestPath2;
+
+namespace DijkstraShortestPath2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DirectedWeightedGraph g = new DirectedWeightedGraph();
+            g.InsertVertex("Zero");
+            g.InsertVertex("One");
+            g.InsertVertex("Two");
+            g.InsertVertex("Three");
+            g.InsertVertex("Four");
+            g.InsertVertex("Five");
+            g.InsertVertex("Six");
+            g.InsertVertex("Seven");
+            g.InsertVertex("Eight");
+
+            g.InsertEdge("Zero", "Three", 2);
+            g.InsertEdge("Zero", "One", 5);
+            g.InsertEdge("Zero", "Four", 8);
+            g.InsertEdge("One", "Four", 2);
+            g.InsertEdge("Two", "One", 3);
+            g.InsertEdge("Two", "Five", 4);
+            g.InsertEdge("Three", "Four", 7);
+            g.InsertEdge("Three", "Six", 8);
+            g.InsertEdge("Four", "Five", 9);
+            g.InsertEdge("Four", "Seven", 4);
+            g.InsertEdge("Five", "One", 6);
+            g.InsertEdge("Six", "Seven", 9);
+            g.InsertEdge("Seven", "Three", 5);
+            g.InsertEdge("Seven", "Five", 3);
+            g.InsertEdge("Seven", "Eight", 5);
+            g.InsertEdge("Eight", "Five", 3);
+            g.FindPaths("Zero");
+            Console.ReadLine();
+        }
+    }
+}
